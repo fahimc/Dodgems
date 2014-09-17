0c9ead8dd59285ce5d4745f4c9d6dc12511ef37d
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2014-09-17 23:26:15 +0100

    socket.io

--- App/js/main.js
+++ App/js/main.js
@@ -1,10 +1,12 @@
 var Main = {
+  username:'Fahim',
   connection:null,
   id:null,
   playersCollection:null,
   players:[],
   x:0,
   y:0,
+  socket:null,
   init:function(){
     document.addEventListener('DOMContentLoaded',this.onLoad.bind(this));
   },
@@ -13,11 +15,32 @@ var Main = {
     this.addEvents();
   },
   createSocket:function(){
-   this.connection = new window.WebSocket('ws://apps.m8e.co.uk:8001');
-   this.connection.onopen = this.onOpen.bind(this);
-   this.connection.onmessage = this.onMessage.bind(this);
- },
- addEvents:function(){
+   //this.connection = new window.WebSocket('ws://apps.m8e.co.uk:8001');
+  // this.connection.onopen = this.onOpen.bind(this);
+  // this.connection.onmessage = this.onMessage.bind(this);
+  this.socket = io();
+  this.socket.on('connect',this.onConnect.bind(this));
+  this.socket.on('ID',this.onID.bind(this));
+  this.socket.on('update',this.onUpdate.bind(this));
+  this.socket.on('message',this.onMessage.bind(this));
+},
+onConnect:function(){
+  console.log('connect');
+},
+onID:function(data){
+  this.id=data;
+  
+  this.socket.emit('message',JSON.stringify({type:'username',id:this.id,name:this.username}));
+},
+onUpdate:function(data){
+ var data = JSON.parse(data);
+ //console.log(data);
+ console.log(data);
+ this.playersCollection = data;
+ this.createPlayers();
+ this.removePlayers();
+},
+addEvents:function(){
   document.getElementById('upButton').addEventListener('mousedown',this.onUpPressed.bind(this));
   document.getElementById('downButton').addEventListener('mousedown',this.onDownPressed.bind(this));
   document.getElementById('leftButton').addEventListener('mousedown',this.onLeftPressed.bind(this));
@@ -26,19 +49,20 @@ var Main = {
 onDownPressed:function(){
   this.y+=10;
   console.log(this.x,this.y);
-  this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]}));
+ // this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]}));
+ this.socket.emit('message',JSON.stringify({type:'position',id:this.id,data:[this.x,this.y]}));
 },
 onUpPressed:function(){
   this.y-=10;
-  this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]}));
+  this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]})); this.socket.emit('message',JSON.stringify({type:'position',id:this.id,data:[this.x,this.y]}));
 },
 onLeftPressed:function(){
   this.x-=10;
-  this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]}));
+   this.socket.emit('message',JSON.stringify({type:'position',id:this.id,data:[this.x,this.y]}));
 },
 onRightPressed:function(){
   this.x+=10;
-  this.connection.send(JSON.stringify({type:"broadcast",data:[this.x,this.y]}));
+   this.socket.emit('message',JSON.stringify({type:'position',id:this.id,data:[this.x,this.y]}));
 },
 onOpen:function(){
 
@@ -58,10 +82,12 @@ test:function(){
 },
 onMessage:function(event){
  var data = JSON.parse(event.data);
+ console.log('onMessage');
  switch(data.type){
   case "ID":
   this.id = data.id;
-  break;
+  console.log(this.id);
+  break;/*
   case "message":
   this.movePlayer(data.data,data.senderId);
   break;
@@ -69,14 +95,14 @@ onMessage:function(event){
   this.playersCollection=data.players;
   this.createPlayers();
   this.removePlayers();
-  break;
+  break;*/
 }
 },
 onClose:function(){
 
 },
 movePlayer:function(data,id){
-  var xy = data.data;
+  var xy = data;
   console.log(xy,id,this.id,this.players[id]);
   this.players[id].style.left=xy[0]+"px";
   this.players[id].style.top=xy[1]+"px";
@@ -84,20 +110,23 @@ movePlayer:function(data,id){
 createPlayers:function(){
   var playground = document.getElementById('playground');
   for(var id in this.playersCollection){
+    var nameElem=document.getElementById("name_"+id);
     if(!this.players[id])
     {
       var player = document.createElement('DIV');
-      var name = document.createElement('DIV');
-      name.innerHTML = "<p>"+this.playersCollection[id].config.username+"</p>";
+      nameElem = document.createElement('DIV');
+      nameElem.id="name_"+id;
       player.id= id;
       player.classList.add('player');
-      player.appendChild(name);
+      player.appendChild(nameElem);
       playground.appendChild(player);
       this.players[id]  = player;
-      if(this.playersCollection[id].currentData)
-      {
-        this.movePlayer(this.playersCollection[id].currentData,id);
-      }
+    }
+    if(this.playersCollection[id].name)nameElem.innerHTML = "<p>"+this.playersCollection[id].name+"</p>";
+    console.log(this.playersCollection[id]);
+    if(this.playersCollection[id].position)
+    {
+      this.movePlayer(this.playersCollection[id].position,id);
     }
 
   }
